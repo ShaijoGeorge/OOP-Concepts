7f205bfccbb33e46d7207d7c4b3f611ccd3eb0f2
Shaijo George <97226113+ShaijoGeorge@users.noreply.github.com>
2023-01-20 18:03:22 +0530

    Add files via upload

---
+++ oopclass.java
@@ -0,0 +1,10 @@
+public class oopclass
+{
+    String x="Hey";
+  
+    public static void main(String[] args)
+    {
+      oopclass o1= new oopclass();
+      System.out.println(o1.x);
+    }
+  }
